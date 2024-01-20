57f2a76c9e6328d73c1729ada5313f7decf61ecc
hmssameer55 <hmssameer55@gmail.com>
2024-01-20 16:42:21 +0000

    responsive adjustments

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,10 @@ import anime from "@/public/anime.png"
 
 export default function Hero() {
   return (
-    <header className="bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
+    <header className="
+     bg-hero bg-center bg-cover bg-no-repeat 
+     p-12 pb-0 md:py-16 md:px-8 flex justify-center 
+     lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
       <div className="flex-1 flex flex-col gap-10">
         <Image
           src={logo}
